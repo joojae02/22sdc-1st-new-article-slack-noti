f4aa72f7561d9f83a30bc7e54608e1382d1bc624
wnwogus <56969647+wnwogus@users.noreply.github.com>
2022-08-25 16:01:09 +0900

    2022-08-25

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import time
+from datetime import datetime, timedelta
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 #from webdriver_manager.chrome import ChromeDriverManger
@@ -9,6 +10,9 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 
+today_year = datetime.today().year   # 현재 연도 가져오기
+today_month = datetime.today().month # 현재 월 가져오기
+today_date = datetime.today().day    # 현재 일 가져오기
 # 조양마트
 # 상단 목록 열기
 # 목록 1page 2page 요소 title과 url 읽기
@@ -61,3 +65,46 @@ time.sleep(1)
 get_title_url_from_table(web_driver) # 두번째 목록 읽기
 
 print(title_url_dic)
+
+def week_no(y, m, d):
+    # 연월일을 입력받아 해당 요일의 주차를 얻는 함수
+    def _ymd_to_datetime(y, m, d): # 3
+        s = f'{y:04d}-{m:02d}-{d:02d}'
+        return datetime.strptime(s, '%Y-%m-%d')
+
+    target_day = _ymd_to_datetime(y, m, d) # 4
+    firstday = target_day.replace(day=1) 
+
+    while firstday.weekday() != 0: 
+        firstday += timedelta(days=1)
+      
+    if target_day < firstday: 
+        return 0
+  
+    return (target_day - firstday).days // 7 + 1 
+
+today_week = week_no(today_year, today_month, today_date)
+month_week_format = '{month}월 {week}주차'.format(month = today_month, week = today_week)
+
+print(month_week_format)
+
+month_week_format = '8월 1주차'
+
+
+# n월 n주자 입고리스트 - 
+for t in title_list:
+    if month_week_format in t :
+        
+        print(t + ' :' + title_url_dic[t])
+        break
+    
+web_driver.get(title_url_dic[t])
+# print(web_driver.text)
+time.sleep(3)
+
+# web_driver.switch_to.frame('mainFrame')
+wine_table_block = web_driver.find_element(By.XPATH,'//*[@id="SE-a6fd6072-e4ae-41e9-b5ad-946a8fa5de15"]/div/div/blockquote/div[2]')
+wine_table_list = wine_table_block.find_elements(By.TAG_NAME, 'p')
+for w in wine_table_list:
+    print(w.text)
+
