62fafa022d49fe72eba634f7d7350a898393d269
wnwogus <56969647+wnwogus@users.noreply.github.com>
2022-08-29 23:04:10 +0900

    2022-08-29

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ def main() :
     
     
     '''
-    slack_token = "xoxb-4004765306050-4007235202500-K9XQWfc1Ia8cO3PqgkfeZvcG"
-    slack_bot = SlackBot()
-    slack_bot.post_message("Hello World!")
+    
     '''
     joyang_blog = JoyangBlog('조양', 'https://blog.naver.com/joyangmart')
     joyang_blog.open_web_driver()
@@ -21,6 +19,9 @@ def main() :
     joyang_blog.read_content()
     joyang_blog.print_wine_list()
 
+    slack_bot = SlackBot(joyang_blog)
+    slack_bot.post_message()
+
     smdcmart_blog = SmdcmartBlog('구판장', 'https://blog.naver.com/smdcmart')
     smdcmart_blog.open_web_driver()
     smdcmart_blog.switch_to_frame('mainFrame')
@@ -29,6 +30,10 @@ def main() :
     smdcmart_blog.access_to_post()
     smdcmart_blog.read_content()
     smdcmart_blog.print_wine_list()
+
+    slack_bot1 = SlackBot(smdcmart_blog)
+    slack_bot1.post_message()
+
     
 main()
 
--- slack.py
+++ slack.py
@@ -2,9 +2,13 @@ import requests
 import json
 
 class SlackBot :
-    def __init__(self) :
+    def __init__(self, blog) :
         self.url = "https://hooks.slack.com/services/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"
-    def post_message(self, text):
+        self.blog = blog
+        self.content_list = self.blog.get_wine_list()
+            
+    def post_message(self):
+        text = ' '.join(str(s) for s in self.content_list)
         data = {'text':text}
         resp = requests.post(url=self.url, json=data)
         return resp
